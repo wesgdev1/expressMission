04a15a61adcde1e9c65c2c267f65a2aa08a685c8
welinton <wesgdeveloper@gmail.com>
2023-05-16 17:10:36 -0500

    se crea servidor en index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const PORT = 3001;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+app.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
+});
